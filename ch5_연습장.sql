637a6ae5de390740bcbdc677dd5079b2d9d835a3
이상용 <lsy3709@naver.com>
2025-05-19 11:29:24 +0900

    3교시, sql 기본 문법 5-2 AND , OR 논리 미니실습

--- ch5_연습장.sql
+++ ch5_연습장.sql
@@ -29,4 +29,43 @@ ORDER BY DEPTNO ASC;
 SELECT * FROM EMP 
 WHERE DEPTNO != 10;
 
+--------------------------------------
+-- 5-2 AND, OR 논리 조건, 다중 조건 
+
+SELECT * FROM EMP 
+WHERE 
+DEPTNO = 30 
+AND 
+JOB = 'SALESMAN';
+
+-- OR 조건 
+-- 하나라도 만족하면 출력됨. 
+-- JOB 가 CLERK 또는 MANAGER 인 사원 출력 해보기
+SELECT * FROM EMP
+WHERE JOB = 'CLERK' OR JOB = 'MANAGER';
+
+--괄호 사용 (우선순위 명확히 하기)
+-- 부서 번호가 10 또는 20이고, 급여가 2000 초과인 경우 
+SELECT * FROM EMP
+WHERE (DEPTNO = 10 OR DEPTNO = 20)
+AND SAL > 2000;
+
+-- 퀴즈1 
+-- 급여가 1500 이상이고, 
+-- 커미션이 NULL 이 아닌 사원만 조회
+-- 힌트) NULL 이 아닌 표현 : IS NOT NULL
+
+-- 퀴즈2
+-- 직무가 'SALESMAN' 이거나, 급여가 3000 이상인 사원 출력
+
+-- 퀴즈3
+-- 부서번호가 10,20,30 중 하나이고, 
+-- 급여가 2000 이상인 사원 출력
+-- 힌트) 10,20,30 중 하나 표현 : IN (10, 20, 30) 이용
+
+
+
+
+
+
 
