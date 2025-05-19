ceffc573ea0f9e6628c04c07fa4df2f8ae40b4cc
이상용 <lsy3709@naver.com>
2025-05-19 11:10:57 +0900

    3교시, sql 기본 문법 5-1 WHERE 미니실습 확인

--- ch5_연습장.sql
+++ ch5_연습장.sql
@@ -10,10 +10,23 @@ WHERE JOB = 'SALESMAN';
 
 --퀴즈1
 -- 급여가(SAL) 2000 이상인 사원만 조회하기 
-
+SELECT * FROM EMP 
+WHERE SAL >= 2000
+ORDER BY SAL ;
 --퀴즈2 
 -- 입사일(HIREDATE)이 '1981-02-20' 이후인 사원만 조회하기
-
+-- 날짜가 이후이면, 값으로 치면 큰값, 최신이다.
+SELECT * FROM EMP 
+WHERE HIREDATE > '1981-02-20'
+ORDER BY HIREDATE ASC;
 --퀴즈3 
 -- 부서 번호가 10이 아닌 사원만 조회 하기. 
 -- 아니다는 표현이 일단은 '<>'로 표기 하기 
+SELECT * FROM EMP 
+WHERE DEPTNO <> 10
+ORDER BY DEPTNO ASC;
+
+SELECT * FROM EMP 
+WHERE DEPTNO != 10;
+
+
