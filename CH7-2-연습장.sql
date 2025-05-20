c2fccc414be05d363740af9f63a7539c8633bf6e
이상용 <lsy3709@naver.com>
2025-05-20 16:33:11 +0900

    7교시, GROUP BY 절, 미니실습 확인

--- CH7-2-연습장.sql
+++ CH7-2-연습장.sql
@@ -30,12 +30,26 @@ ORDER BY AVG(SAL) DESC;
 -- 부서 번호와 직책별 평균 급여를 출력하고, 
 -- 평균 급여 기준 내림차순 정렬하시오.   
 -- 별칭 : 평균 급여
+SELECT DEPTNO, JOB, AVG(SAL) AS "평균 급여"
+FROM EMP 
+GROUP BY DEPTNO, JOB
+ORDER BY AVG(SAL) DESC;
 
 --퀴즈2
 -- `GROUP BY` 절에 포함되지 않은 `ename`을 
 -- `SELECT`에서 사용한 경우의 오류 상황을 만들어 보시오.  
 -- 
+SELECT DEPTNO, JOB, ENAME, AVG(SAL) AS "평균 급여"
+FROM EMP 
+GROUP BY DEPTNO, JOB
+ORDER BY AVG(SAL) DESC;
 
 --퀴즈3
 -- 각 부서별로 보너스가 있는 사원의 수를 구하시오. 
 -- 별칭 : 보너스 받는 직원수
+SELECT DEPTNO, COUNT(COMM) AS "보너스 받는 직원수"
+FROM EMP 
+GROUP BY DEPTNO;
+
+
+
