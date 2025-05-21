75d38a2d7e72ff9bf27c9f83b99ff2842643421f
이상용 <lsy3709@naver.com>
2025-05-21 14:40:52 +0900

    5교시, 비등가 조인, 외부조인 , 미니실습

--- CH8-2-연습장.sql
+++ CH8-2-연습장.sql
@@ -14,3 +14,52 @@ SELECT E.ENAME, D.DNAME
 FROM EMP E, DEPT D
 WHERE E.DEPTNO = D.DEPTNO(+);
 
+--원본, 자체 조인 = 등가 조인, MGR = EMPNO
+-- NULL  인 경우  , 데이터가 누락이됨.
+SELECT * FROM EMP;
+SELECT 
+E.EMPNO AS "EMP 사원번호", 
+E.ENAME AS "EMP 사원명", 
+E.MGR AS "EMP직속 상관번호",
+M.ENAME AS "EMP2직속 상관명"
+FROM EMP E, EMP M
+WHERE E.MGR = M.EMPNO;
+
+-- 외부 조인 버전으로 변경해서 누락 없이 
+-- 왼쪽 컬럼을 기준으로 표기하기. 
+-- 왼쪽 외부 조인
+
+SELECT 
+E.EMPNO AS "EMP 사원번호", 
+E.ENAME AS "EMP 사원명", 
+E.MGR AS "EMP직속 상관번호",
+M.ENAME AS "EMP2직속 상관명"
+FROM EMP E, EMP M
+WHERE E.MGR = M.EMPNO(+);
+
+
+
+-- 외부 조인 버전으로 변경해서 누락 없이 
+-- 오른쪽 컬럼을 기준으로 표기하기. 
+-- 오른쪽 외부 조인
+
+SELECT 
+E.EMPNO AS "EMP 사원번호", 
+E.ENAME AS "EMP 사원명", 
+E.MGR AS "EMP직속 상관번호",
+M.ENAME AS "EMP2직속 상관명"
+FROM EMP E, EMP M
+WHERE E.MGR(+) = M.EMPNO;
+
+
+--퀴즈 1,
+-- EMP와 DEPT 테이블에서 부서가 없는 사원도 포함해 
+-- 사원명과 부서명을 출력하시오 (왼쪽 외부 조인)
+
+
+--퀴즈 2
+-- 오른쪽 외부 조인을 사용하여 부서가 있지만, 
+-- 사원이 없는 부서를 포함해서 출력하시오. 
+
+--퀴즈 3
+-- WHERE 절에 추가 조건(`job = 'CLERK'`)을 넣고 부서별 사원 출력  
