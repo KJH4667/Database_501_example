469133e199473e1995b84986fa187c202f18d6f8
이상용 <lsy3709@naver.com>
2025-05-21 12:34:33 +0900

    4교시, 등가 조인 , 미니실습

---
+++ CH8-1-연습장.sql
@@ -0,0 +1,34 @@
+-- 조인 사용 하기 전에 문제점 제시, 
+-- 카티션 곱 , 
+SELECT * FROM EMP; -- 몇개 : 14개
+SELECT * FROM DEPT; -- 몇개 : 10개
+-- KING , DEPTNO 10, DNAME : ACCOUNTING , 
+-- 이거 외에 다른 부서도 또 출력이됨. 중복이됨. 
+SELECT E.ENAME, D.DNAME  -- 총: 140개 
+FROM EMP E, DEPT D;
+
+-- 테이블 별칭 이용해서, 조인 해보기. 
+SELECT E.ENAME, D.DNAME , D.LOC
+FROM EMP E, DEPT D
+WHERE E.DEPTNO = D.DEPTNO ;
+
+-- EMP와 DEPT 테이블 등가 조인하여 부서번호가 30번인
+-- 사원만 출력해보기. 
+SELECT E.ENAME, D.DNAME 
+FROM EMP E, DEPT D 
+WHERE E.DEPTNO = D.DEPTNO 
+AND E.DEPTNO = 30;
+
+
+--퀴즈 1
+-- EMP 와 DEPT 테이블 조인하여 관리자(MANAGER)직무를 가진 사원의 이름과 
+-- 부서명 출력 해보기.
+-- 별칭 : 사원명, 부서명 
+
+--퀴즈 2, 힌트 : 같은 테이블을 활용해보기.
+-- 각 사원의 이름과 그 사원의 직속 상관의 이름을 함께 출력해보기. 
+-- 별칭 : 사원명, 직속 상관명
+
+
+
+
