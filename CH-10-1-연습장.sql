837ef711ca08379d15f69161742b5547d710d37d
이상용 <lsy3709@naver.com>
2025-05-22 17:33:53 +0900

    8교시, 기본 INSERT 예시, 테이블 복사하기, 조회해서 테이블에 입력하기

--- CH-10-1-연습장.sql
+++ CH-10-1-연습장.sql
@@ -25,9 +25,37 @@ SELECT * FROM EMP;
 
 -- 퀴즈1, 
 -- DEPT 테이블에 (99, ‘AI팀’, ‘JEJU’) 데이터를 추가하시오. 
- 
+ INSERT INTO dept (deptno, dname, loc) VALUES (99, 'AI팀', 'JEJU');
+ SELECT * FROM dept;
 -- 퀴즈2, 
 -- EMP 테이블에 사번 1234, 이름 'LEE', 입사일을 SYSDATE로 추가하시오.
-  
+  INSERT  INTO emp (empno, ename, hiredate) VALUES (1234, 'LEE', SYSDATE);
+  SELECT * FROM emp;
 -- 퀴즈3, 
 -- DEPT에 NULL을 포함한 값 삽입 
+-- 테이블에 PK 값 의무적으로 입력하기, 
+-- PK : NOT NULL, UNIQUE 제약조건이 있는 열
+INSERT INTO dept (deptno, dname, loc) VALUES (NULL, 'AI팀', NULL);
+SELECT * FROM dept;
+
+-- 데이터 사전에서 테이블의 제약조건 확인해보기 DEPT 테이블
+SELECT * FROM user_constraints WHERE table_name = 'DEPT';
+-- 제약조건 확인하기
+
+-- 테이블 복사하는 방법 
+-- EMP 테이블에서 부서번호가 30인 사원 데이터를 
+-- EMP_TEMP_30 테이블에 복사하기
+-- 새로운 테이블을 생성하는 기본 문법 
+-- CREATE TABLE 테이블명 AS SELECT * FROM 테이블명 WHERE 조건;
+-- EMP_TEMP_30 테이블 생성 , 단순 빈테이블 생성
+CREATE TABLE EMP_TEMP_30 AS SELECT * FROM EMP WHERE 1=0;
+-- 실제 데이터 삽입
+CREATE TABLE EMP_TEMP_40 AS SELECT * FROM EMP WHERE 1=1;
+
+INSERT INTO EMP_TEMP_30
+SELECT * FROM EMP WHERE deptno = 30;
+
+-- EMP_TEMP_30 테이블 확인
+SELECT * FROM EMP_TEMP_30;
+-- EMP_TEMP_40 테이블 확인
+SELECT * FROM EMP_TEMP_40;
