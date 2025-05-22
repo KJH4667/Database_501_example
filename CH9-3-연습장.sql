3a8c437d9c8eb9050c16ff1f71c17400fdf7fe09
이상용 <lsy3709@naver.com>
2025-05-22 11:14:47 +0900

    3교시, 서브쿼리 다중행, 기본 문법 미니실습

--- CH9-3-연습장.sql
+++ CH9-3-연습장.sql
@@ -1,7 +1,8 @@
 -- 실행 결과가 여러 객인 다중행 서브쿼리 
 
 -- 기본 문법 
--- IN : 복수의 값 중 하나와 일치하는 경우 
+-- IN 
+-- : 복수의 값 중 하나와 일치하는 경우 
 -- DALLAS 에 위치한 부서의 부서번호 중 
 -- 하나에 속한 사원의 이름을 출력 예시, 
 SELECT * FROM DEPT ;
@@ -25,6 +26,54 @@ WHERE SAL < ANY (
 );
 
 
+-- ALL 
+-- 부서 번호 30번의 모든 사원보다 급여가 많은 사원의 
+-- 이름 출력
+-- ALL 전부 만족해야 참이 되는 조건, 
+-- 가장 큰 값보다 더 커야함. 
+-- MAX()와 비슷한 족건으로 사용한다. 
+SELECT ENAME FROM EMP 
+WHERE SAL > ALL (
+    SELECT SAL FROM EMP WHERE DEPTNO = 30
+);
+
+--EXISTS 연산자 
+-- 사원이 소속하는 부서의 이름을 출력 
+-- 하나라도 존재하면 참(TRUE) 
+-- 매우 빠른 조건에 존재 확인에 유리(데이터 확인 아님)
+-- 서브쿼리의 실제 데이터가 아니라 존재 유무만 확인함. 
+-- 반환값이 있으면 무조건 TRUE
+SELECT DNAME FROM DEPT D 
+WHERE EXISTS (
+ SELECT * FROM E
+ WHERE E.DEPTNO = D.DEPTNO
+);
+
+-- 30번 부서 사원들과 동일한 급여를 받는 사원 
+SELECT ENAME, SAL FROM EMP 
+WHERE SAL IN (
+    SELECT SAL FROM EMP WHERE DEPTNO = 30
+);
+
+-- 30번 부서 중 가장 높은 급여보다 작게 급여를 맞는 직원
+SELECT ENAME, SAL FROM EMP 
+WHERE SAL < ALL (
+    SELECT SAL FROM EMP WHERE DEPTNO = 30
+);
+
+-- 부서별 최대 급여 출력 
+SELECT DEPTNO, MAX(SAL) FROM EMP 
+GROUP BY DEPTNO ;
+SELECT * FROM EMP;
+
+-- 퀴즈1, 
+-- 30번 부서의 최소 급여보다 많은 급여를 받는 사원 출력  
+ 
+-- 퀴즈2, 
+-- 30번 부서의 최대 급여보다 낮은 급여를 받는 사원 출력  
+  
+-- 퀴즈3, 
+-- EMP 테이블에 소속된 사원이 있는 부서의 이름을 출력 (`EXISTS`) 
 
 
 
