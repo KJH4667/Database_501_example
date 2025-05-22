36e26f328906db10b4b4528d9b17b3da20dc608c
이상용 <lsy3709@naver.com>
2025-05-22 14:13:48 +0900

    5교시, FROM 절에서 사용하는 서브쿼리 , 인라인 WITH,미니실습 확인

--- CH9-5-연습장.sql
+++ CH9-5-연습장.sql
@@ -35,15 +35,35 @@ SELECT * FROM EMP WHERE DEPTNO = (
 SELECT ENAME,JOB FROM EMP30;
 
 -- 퀴즈1, 
--- 부서별 평균 급여가 3000 이상인 부서의 
+-- 부서별 평균 급여가 2000 이상인 부서의 
 -- 직책별 사원 수 출력 (WITH절 + GROUP BY)  
+WITH DEPT_AVG AS (
+    SELECT DEPTNO, AVG(SAL) AS "평균급여" FROM EMP 
+    GROUP BY DEPTNO 
+    HAVING AVG(SAL) >= 2000
+)
+SELECT E.DEPTNO , E.JOB, COUNT(*) AS "직책별 사원 수"
+FROM EMP E 
+JOIN DEPT_AVG D ON E.DEPTNO = D.DEPTNO 
+GROUP BY E.DEPTNO, E.JOB;
  
 -- 퀴즈2, 
--- 부서별 최고 급여자를 인라인 뷰로 추출하고, 사원명과 급여 출력  
+-- 부서별 최고 급여자를 인라인 뷰로 추출하고, 
+-- 사원명과 급여 출력  
+SELECT ENAME, SAL FROM EMP 
+WHERE (SAL, DEPTNO) IN (
+    SELECT MAX(SAL), DEPTNO FROM EMP 
+    GROUP BY DEPTNO
+);
   
 -- 퀴즈3, 
--- WITH절로 정의된 사원 리스트에서, 급여가 평균 이상인 사원만 출력  
-
+-- WITH절로 정의된 사원 리스트에서, 
+-- 급여가 평균 이상인 사원만 출력  
+WITH EMP20 AS (
+SELECT * FROM EMP WHERE DEPTNO = 20
+)
+SELECT ENAME ,SAL FROM EMP20 
+WHERE SAL >= (SELECT AVG(SAL) FROM EMP20);
 
 
 
