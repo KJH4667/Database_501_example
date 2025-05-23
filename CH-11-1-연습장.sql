66967a49b67e7f72487df2c4b0feb7b5fb2b4e90
이상용 <lsy3709@naver.com>
2025-05-23 16:26:54 +0900

    7교시,트랜잭션 기본 미니실습 확인

--- CH-11-1-연습장.sql
+++ CH-11-1-연습장.sql
@@ -23,16 +23,28 @@ SAVEPOINT SP1;
 
 -- 순서5, 특정 지점으로 롤백 
 ROLLBACK TO SP1;
-
+SELECT * FROM emp_copy2;
+CREATE TABLE emp_copy2 AS SELECT * FROM emp;
 -- EMP -> EMP_COPY2 테이블 복사를 먼저 진행 후, 
 -- 퀴즈1, 
 -- 부서번호가 20인 사원들의 급여를 10% 인상 후, 
 -- 조건에 따라 되돌릴 수 있도록 SAVEPOINT (SP2)를 설정하시오. 
- 
+ UPDATE emp_copy2 SET sal = sal * 1.1 WHERE deptno = 20;
+ COMMIT;
 -- 퀴즈2, 
--- 사번이 7839인 사원의 급여를 5000으로 변경하고, 
+-- 사번이 7839인 사원의 급여를 8000으로 변경하고, 
 -- 이 작업만 ROLLBACK하시오.
-  
+  SAVEPOINT SP2;
+  SELECT * FROM emp_copy2 WHERE empno = 7839;
+  UPDATE emp_copy2 SET sal = 8000 WHERE empno = 7839;
+  ROLLBACK TO SP2;
+  COMMIT;
+
 -- 퀴즈3, 
 -- 여러 UPDATE 작업 수행 후 COMMIT하지 않고
 -- 전체를 ROLLBACK 하시오.
+UPDATE emp_copy2 SET sal = sal * 1.1 WHERE deptno = 10;
+UPDATE emp_copy2 SET sal = sal * 1.1 WHERE deptno = 20;
+UPDATE emp_copy2 SET sal = sal * 1.1 WHERE deptno = 30;
+-- COMMIT OR ROLLBACK;
+ROLLBACK;
