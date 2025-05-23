05e80b79941a9c5b78c3ee9bdf8a69d1b6658952
이상용 <lsy3709@naver.com>
2025-05-23 15:30:51 +0900

    6교시, 트랜잭션 기본 미니실습

---
+++ CH-11-1-연습장.sql
@@ -0,0 +1,38 @@
+-- 하나의 단위로 데이터를 처리하는 트랜잭션 
+
+-- 기본문법 
+-- 데이터 변경 
+select * from emp_copy;
+-- EMP_COPY 테이블에 원본 EMP 테이블 복사
+INSERT INTO emp_copy
+SELECT * FROM emp;
+COMMIT;
+
+-- EMP_COPY 순서1, 테이블 테이터 변경 
+UPDATE emp_copy SET sal = sal + 10000
+WHERE deptno = 10;
+
+-- 순서2, 트랜잭션 저장 
+COMMIT;
+
+-- 순서3, 트랜잭션 취소 
+ROLLBACK;
+
+-- 순서4, 특정 지점으로 설정
+SAVEPOINT SP1;
+
+-- 순서5, 특정 지점으로 롤백 
+ROLLBACK TO SP1;
+
+-- EMP -> EMP_COPY2 테이블 복사를 먼저 진행 후, 
+-- 퀴즈1, 
+-- 부서번호가 20인 사원들의 급여를 10% 인상 후, 
+-- 조건에 따라 되돌릴 수 있도록 SAVEPOINT (SP2)를 설정하시오. 
+ 
+-- 퀴즈2, 
+-- 사번이 7839인 사원의 급여를 5000으로 변경하고, 
+-- 이 작업만 ROLLBACK하시오.
+  
+-- 퀴즈3, 
+-- 여러 UPDATE 작업 수행 후 COMMIT하지 않고
+-- 전체를 ROLLBACK 하시오.
