ebadd4340d39527c1c3d41c7e7613e6b4e39f100
이상용 <lsy3709@naver.com>
2025-05-26 15:38:14 +0900

    6교시, 시퀀스 , 미니실습 확인

--- CH-13-4-연습장.sql
+++ CH-13-4-연습장.sql
@@ -58,12 +58,35 @@ ALTER SEQUENCE EMP_SEQ
 
 -- 연습용 테이블 생성 후 작업하기. 
 -- DEPT -> DEPT_SEQ_TEST 테이블 복사 후 작업. 
+CREATE TABLE DEPT_SEQ_TEST AS SELECT * FROM DEPT WHERE 1=0;
 -- 퀴즈1, 
 -- 1부터 시작하는 DEPT_SEQ 시퀀스를 생성하시오.  
 -- 증감 10씩, 맥스 : 999999999, NOCYCLE 옵션 사용. 
+CREATE SEQUENCE DEPT_SEQ
+START WITH 1
+INCREMENT BY 10
+MAXVALUE 999999999
+NOCYCLE;
+-- 시퀀스 확인
+SELECT DEPT_SEQ.NEXTVAL FROM DUAL;
  
 -- 퀴즈2, 
 -- DEPT_SEQ_TEST, 샘플 데이터를 추가해서, 증감값 확인,.(자동증가 확인이 목적)
-  
+DESC DEPT_SEQ_TEST; -- 추가시, 컬럼의 이름을 생략 하게 되면, 정의된 컬럼 순서대로 입력하기.
+INSERT INTO DEPT_SEQ_TEST VALUES(
+    DEPT_SEQ.NEXTVAL, -- deptno 자동 생성
+    '개발부',         -- dname 문자열 타입
+    '서울'            -- loc 문자열 타입
+);
+SELECT * FROM DEPT_SEQ_TEST;
 -- 퀴즈3, 
 -- 마지막으로 생성된 시퀀스 번호 확인 및 증감 10 -> 100 변경도 해보고, 삭제도 해보기
+-- 정의가 NUMBER(2) -> 수정 해보기. 
+ALTER TABLE DEPT_SEQ_TEST MODIFY (deptno NUMBER(3));
+ALTER SEQUENCE DEPT_SEQ
+    INCREMENT BY 100; -- 증감값 변경
+-- 시퀀스 삭제 
+DROP SEQUENCE DEPT_SEQ;    
+-- 시퀀스 확인 
+SELECT * FROM USER_SEQUENCES WHERE SEQUENCE_NAME = 'DEPT_SEQ';
+-- 시퀀스가 삭제되었는지 확인
