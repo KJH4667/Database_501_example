31fb276836d2410076cf4648698593ae9ba91fde
이상용 <lsy3709@naver.com>
2025-05-27 10:23:31 +0900

    2교시, CH14-3, 제약조건, UNIQUE, 미니실습 확인

--- CH-14-3-연습장.sql
+++ CH-14-3-연습장.sql
@@ -53,11 +53,54 @@ INSERT INTO TABLE_UNIQUE(ID, NAME, EMAIL) VALUES(
 -- 퀴즈1, 
 -- 테이블 생성 시 UNIQUE 지정 해보기, 방법 1, 2,3 
 -- 테이블명 :  user_table, 컬럼, user_id에 유니크 설정 
- 
+DROP TABLE user_table;
+ CREATE TABLE user_table(
+    ID NUMBER(5) PRIMARY KEY,
+    NAME VARCHAR2(20) NOT NULL,
+    -- 방법1
+    EMAIL VARCHAR2(50) CONSTRAINT email_unique_mini UNIQUE,
+    -- 방법2
+    EMAIL2 VARCHAR2(50) UNIQUE,
+    EMAIL3 VARCHAR2(50) 
+
+);
+-- 방법3
+ALTER TABLE user_table ADD CONSTRAINT email_unique_mini2 UNIQUE (EMAIL3);
+-- 제약 조건 확인
+SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'USER_TABLE';
+
+-- 테이블에 컬럼 추가 user_id
+ALTER TABLE user_table ADD USER_ID VARCHAR2(20) UNIQUE;
+
+
+
 -- 퀴즈2, 
 -- 테이블 생성 후, 제약 조건 추가 , 
 -- 테이블명 :  user_table2, 컬럼, user_id에 유니크 설정 
-  
+DROP TABLE user_table2;
+   CREATE TABLE user_table2(
+    ID NUMBER(5) PRIMARY KEY,
+    NAME VARCHAR2(20) NOT NULL,
+    user_id VARCHAR2(50) NOT NULL
+);
+ALTER TABLE user_table2 ADD CONSTRAINT email_unique_mini_q2 UNIQUE (user_id);
+-- 제약 조건 확인
+SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'USER_TABLE2';
 -- 퀴즈3, 
 -- user_table2 , 시퀀스 생성 후, 자동 번호 생성기를 이용해서, 데이터 입력 . 
 -- 중복 테스트도 해보기. 
+CREATE SEQUENCE USER_TABLE2_SEQ
+START WITH 1
+INCREMENT BY 1  
+MAXVALUE 99999
+NOCYCLE;
+-- 시퀀스 생성 확인
+SELECT * FROM USER_SEQUENCES WHERE SEQUENCE_NAME = 'USER_TABLE2_SEQ';
+-- 샘플 데이터 추가
+INSERT INTO user_table2(ID, NAME, USER_ID) VALUES(
+    USER_TABLE2_SEQ.NEXTVAL, '홍길동', 'HONG');
+-- 데이터 조회
+SELECT * FROM user_table2;
+-- 샘플 데이터 추가
+INSERT INTO user_table2(ID, NAME, USER_ID) VALUES(
+    USER_TABLE2_SEQ.NEXTVAL, '홍길동2', 'HONG');
